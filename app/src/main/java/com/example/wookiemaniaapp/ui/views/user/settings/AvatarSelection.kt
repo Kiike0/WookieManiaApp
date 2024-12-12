37edd09d6c1e4debbc13be72f1b7090d6d9de180
Kiike0 <quiquevilrey@gmail.com>
2024-12-12 11:52:52 +0100

    cambios para mejorar la optimización

--- app/src/main/java/com/example/wookiemaniaapp/ui/views/user/settings/AvatarSelection.kt
+++ app/src/main/java/com/example/wookiemaniaapp/ui/views/user/settings/AvatarSelection.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -25,11 +27,13 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import coil.compose.rememberImagePainter
+import com.example.wookiemaniaapp.R
 import com.example.wookiemaniaapp.cabeceratipo3.firaSans
 import com.example.wookiemaniaapp.navigation.Routes
 import com.example.wookiemaniaapp.ui.painters.avatarUrlResources
@@ -40,34 +44,56 @@ import com.example.wookiemaniaapp.viewmodels.AvatarViewModel
  * Función composable que representa la pantalla de la selección de avatares.
  *
  * @param navController El controlador de navegación utilizado para navegar en las diferentes pantallas.
+ * @param avatarViewModel El controlador de avatares del viewmodel utilizado para gestionar los avatares de los usuarios.
  */
 @Composable
 fun AvatarSelectionScreen(
-    navController: NavHostController,   // Controlador de navegación para navegar entre pantallas
-    avatarViewModel: AvatarViewModel   // ViewModel para manejar la lógica relacionada con los avatares
+    navController: NavHostController,
+    avatarViewModel: AvatarViewModel
 ) {
-    val context = LocalContext.current  // Obtener el contexto actual para mostrar toasts y otras acciones
+    val context = LocalContext.current  // Obtiene el contexto actual para mostrar toasts y otras acciones
 
     // Estructura de la pantalla
     Column(
         modifier = Modifier
             .fillMaxSize()               // Llena todo el tamaño de la pantalla
-            .background(ColorApp)        // Establece el color de fondo
-            .padding(16.dp),             // Agrega padding alrededor del contenido
+            .background(ColorApp),        // Establece el color de fondo
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Título de la pantalla
-        Text(
-            text = "Selecciona un Avatar",
-            fontFamily = firaSans,       // Familia de fuentes personalizada
-            fontWeight = FontWeight.Bold,// Fuente en negrita
-            fontSize = 24.sp,            // Tamaño de la fuente
-            color = Color.Black,
-            modifier = Modifier.padding(bottom = 16.dp) // Padding debajo del título
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Box(
+                modifier = Modifier
+                    .padding(top = 25.dp, start = 25.dp)
+                    .clickable(onClick = {
+                        navController.navigate(Routes.Profile.route)
+                    })
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.closeicon),
+                    contentDescription = "Settings"
+                )
+            }
+
+            // Título de la pantalla
+            Text(
+                text = "Selecciona un Avatar",
+                fontFamily = firaSans,       // Familia de fuentes personalizada
+                fontWeight = FontWeight.Bold,// Fuente en negrita
+                fontSize = 19.sp,            // Tamaño de la fuente
+                color = Color.Black,
+                modifier = Modifier.padding(start = 60.dp, top = 25.dp) // Padding debajo del título
+            )
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
 
         // Lista que muestra los avatares en filas de dos elementos
+        // Se usa Lazycolumn por si se quieren añadir más avatares
         LazyColumn {
             items(avatarUrlResources.chunked(2)) { rowItems ->   // Divide la lista de URLs en listas de dos elementos
                 Row(
